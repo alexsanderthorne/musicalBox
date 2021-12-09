4ea78aff30e3a91eaef7fe51d827f303fc7f3c7e
alexsanderthorne <alexsanderthorne@gmail.com>
2021-12-09 18:06:29 -0300

    add mario bross theme && asa branca

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,8 @@
-//#include <stdlib.h>
-//#include <stdio.h>
-//#include <piratesOfTheCaribbean>
+// incluindo a biblioteca do LCD
+#include <LiquidCrystal.h>
+
+//Define os pinos que serão utilizados para ligação ao display
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int estadoButtonVerde = 0;
 int estadoButtonAmarelo = 0;
@@ -14,12 +16,17 @@ int ultimoClickButton = 0;
 int buttonPushCounter = 0;
 int controleEstadoButton = 0;
 
+//count = 0;
+
 void setup() {
   pinMode(10, OUTPUT);
   pinMode(buttonPinVerde, INPUT_PULLUP);
   pinMode(buttonPinAmarelo, INPUT_PULLUP);
   Serial.begin(9600);
   Serial.println("Test");
+  lcd.begin(16, 2);
+  lcd.setCursor(3, 0);
+  lcd.print("Music Box");
 }
 
 void loop() {
@@ -27,13 +34,30 @@ void loop() {
   estadoButtonAmarelo = digitalRead(buttonPinAmarelo);
 
   Serial.println(estadoButtonAmarelo);
-  if (digitalRead(buttonPinAmarelo) == LOW) {
-    delay(100);
-    playSongPirates();
+  for (int count = 0; count < 5; ++count) {
+    if (digitalRead(buttonPinAmarelo) == LOW) {
+      delay(100);
+      //playSongPirates();
+    }
+
+    if (digitalRead(buttonPinVerde) == LOW and count == 1) {
+      delay(100);
+      //playSongMarioBross();
+      playSongPirates();
+    }
+    if (digitalRead(buttonPinVerde) == LOW and count == 2) {
+      noTone(10);
+      delay(100);
+      //playSongMarioBross();
+      playSongStarWazz();
+
+    }
+    if (digitalRead(buttonPinVerde) == LOW and count == 3) {
+      noTone(10);
+      delay(100);
+      playSongAsaBranca();
+    }
+    ++count;
   }
 
-  if (digitalRead(buttonPinVerde) == LOW){
-    delay(100);
-    playSongStarWazz();
-  }
 }
